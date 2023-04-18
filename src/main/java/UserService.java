24627721edf7aadc881598b5ba8fd9e0b2a2d5c1
MMamajonov <mamajonov_muhriddin@mail.ru>
2023-04-19 02:58:03 +0600

    Task-006: Убрал неиспользуемые импорты в UserService и UserHandlere

--- src/main/java/UserService.java
+++ src/main/java/UserService.java
@@ -1,68 +1,47 @@
 package main.java;
 
-import java.sql.*;
+import org.hibernate.Query;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+
 import java.util.ArrayList;
 import java.util.List;
 
 public class UserService {
-
     public List<UserEntity> getUsers() {
         List<UserEntity> userList = new ArrayList<>();
 
-        try {
-            // Загрузка драйвера JDBC для mysql
-            Class.forName("com.mysql.jdbc.Driver");
 
-            // Установление соединения с базой данных
-            Connection connection = DriverManager.getConnection(
-                    "jdbc:mysql://localhost:3306/my_bank", "root", "");
+        SessionFactory factory = SessionFactoryMaker.getFactory();
 
-            // Создание объекта Statement
-            Statement statement = connection.createStatement();
-
-            // Выполнение SQL-запроса
-            ResultSet resultSet = statement.executeQuery("SELECT * FROM user_new"); //   в место  селекто инсерт
-
-            // Обработка результатов запроса
-            while (resultSet.next()) {
-                userList.add(new UserEntity(
-                        resultSet.getLong("id"),
-                        resultSet.getString("name"),
-                        resultSet.getString("passnum"),
-                        resultSet.getLong("balans")
-                ));
-                System.out.println(resultSet.getString("name"));
-            }
-
-            // Закрытие соединения, объекта Statement и объекта ResultSet
-            resultSet.close();
-            statement.close();
-            connection.close();
-
-        } catch (ClassNotFoundException | SQLException e) {
-            e.printStackTrace();
+        try (Session session = factory.openSession()) {
+            Transaction tx = session.beginTransaction();
+            Query<UserEntity> query = session.createQuery("from UserEntity", UserEntity.class);
+            userList = query.getResultList();
+        } catch (Exception ex) {
+            ex.printStackTrace();
         }
 
         return userList;
     }
 
-    //todo читаем юзера с бади поста и вставляем в таблицу
-    public List<UserEntity> saveUser(String name, String passNum,String balance){
-        try {
-            Class.forName("com.mysql.jdbc.Driver");
+    public List<UserEntity> saveUser(String n, String i, String st) {
+        UserEntity b = new UserEntity();
+        b.setName(n);
+        b.setPassNum(n);
+        b.getBalance();
+        SessionFactory factory = SessionFactoryMaker.getFactory();
 
-            // Установление соединения с базой данных
-            Connection connection = DriverManager.getConnection(
-                    "jdbc:mysql://localhost:3306/my_bank", "root", "");
-
-            // Создание объекта Statement
-            Statement statement = connection.createStatement();
-
-            // Выполнение SQL-запроса " + val + "
-            statement.executeUpdate("INSERT INTO user_new (name, passnum, balans) VALUES('" + name + "', '" + passNum + "', " + balance + ")");
-        }catch (ClassNotFoundException | SQLException e) {
-            e.printStackTrace();
+        try (Session session = factory.openSession()) {
+            Transaction tx = session.beginTransaction();
+            session.save(b);
+            tx.commit();
+        } catch (Exception ex) {
+            ex.printStackTrace();
         }
+
         return getUsers();
     }
 }
+
