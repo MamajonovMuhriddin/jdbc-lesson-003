bc27030b24cfbb4e39a75d6c1f247af45371bf71
MMamajonov <mamajonov_muhriddin@mail.ru>
2023-04-19 02:00:15 +0600

    Task-006: Убрал неиспользуемые импорты в PetService

--- src/main/java/MyServer.java
+++ src/main/java/MyServer.java
@@ -9,7 +9,7 @@ public class MyServer {
     public static void main(String[] args) {
         try {
             HttpServer server = HttpServer.create(new InetSocketAddress(8080), 0);
-         //   server.createContext("/user", new UserHandler());
+            server.createContext("/user", new UserHandler());
             server.createContext("/pet", new PetHandler()); //todo создать таблицу pet (id, nickname, type), получать список, сохранять одного пета
             server.setExecutor(null);
             server.start();
